3f10536b9c65e2fdbfe0a501d24ace6437d47614
Aswin <aswinv793@gmail.com>
2025-12-03 21:26:20 +0530

    Server file ready

---
+++ backEnd/src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cros'
+const app = express()
+
+
+app.use(cors({
+    origin:'http://localhost:4200',
+    methods:['GET','POST'],
+    credentials:true
+}))
+
+app.get('/',(req,res)=>{
+    res.send("Server is running")
+})
+
+export default app;
